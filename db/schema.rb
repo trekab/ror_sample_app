c2152666d7e8cd248e58272d1a9434046dc5cc90
Treasure Kabareebe <treasurecalm@gmail.com>
2020-08-31 04:23:37 +0300

    User validations: Add uniqueness validation for email address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_004612) do
+ActiveRecord::Schema.define(version: 2020_08_31_011947) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
